5b12d16c45ddffb1a847831fa1ee9127c246afb1
Mohammed Ajil <muy@ajil.ch>
2016-11-24 17:00:24 +0100

    epochs

--- project_02/solution.py
+++ project_02/solution.py
@@ -6,7 +6,7 @@ def transform(X):
     return X
 
 def learning_rate(t):
-	return 1/t
+    return 1/np.sqrt(t)
 
 def mapper(key, value):
     # key: None
@@ -16,18 +16,23 @@ def mapper(key, value):
 
     # initialize weight vector
     w = np.random.randn(400)
-    t = 0
-    for sample in value:
-    	t+=1
-    	sample = sample.split()
-    	y = int(sample[0])
-    	x = np.array(map(float, transform(sample[1:])))
+    for i in range(1):
+        updates = 0
+        t = 0
+        for sample in value:
+            t+=1
+            sample = sample.split()
+            y = int(sample[0])
+            x = np.array(map(float, transform(sample[1:])))
 
-    	if (np.dot(w, x) * y < 1):
-    		# classified wrong
-    		# take a step in the negative gradient direction
-    		w -= learning_rate(t) * y * x
-    		# w *= min(1, 1/np.linalg.norm(w,2))
+            if (np.dot(w, x) * y < 1):
+                # classified wrong
+                # take a step in the negative gradient direction
+                updates += 1
+                w += learning_rate(t) * y * x
+                w *= min(1, 1/np.linalg.norm(w,2))
+        if updates < 1000:
+            break
 
 
     yield "weightvector", w  # This is how you yield a key, value pair
@@ -39,5 +44,5 @@ def reducer(key, values):
     # Note that we do *not* output a (key, value) pair here.
     w = np.zeros(400)
     for value in values:
-    	w += value
+        w += value
     yield w/len(values)
